34e13c65f23554098ca39fd1e6552ddf4a7782c6
Pierre Moinet <pierre.moinet@heig-vd.ch>
2026-09-25 17:04:35 +0200

    Etape 1, securiser les datas

--- Laboratoires/Labo-02/main.cpp
+++ Laboratoires/Labo-02/main.cpp
@@ -1,7 +1,16 @@
 /* --------------------------- 
 Laboratoire : 02
-Auteur(s) : 
-Date : 
+Auteur(s) : Pierre Moinet
+Date : 25.09.2026
 But : Calcul du temps de trajet 
 Remarque(s) : 
 --------------------------- */
+
+#include <iostream>
+using namespace std;
+int main() {
+
+
+
+
+}
